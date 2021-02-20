1c3c62744e0693c1f2dc24b8cfca240f49b8a3bb
DanielRamirezGe <42500282+DanielRamirezGe@users.noreply.github.com>
2021-02-20 01:29:48 -0600

    Create new Proyuect
    Create a new proyect in C#

---
+++ NewProyectHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewProyectHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
